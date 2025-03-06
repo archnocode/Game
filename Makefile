5eeeb684282dda65b14b1646fb27cc41bd458369
Nocode <arhcnocode@gmail.com>
2025-03-06 20:43:01 +0300

    Deleted odd files

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ SRC = $(wildcard $(PREF_SRC)*.cpp)
 OBJ = $(patsubst $(PREF_SRC)%.cpp, $(PREF_OBJ)%.o, $(SRC))
 
 $(TARGET) : $(OBJ)
-	$(CC) $(OBJ) -o $(TARGET) -lsfml-graphics -lsfml-window -lsfml-system
+	$(CC) $(OBJ) -o $(TARGET) -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -lsfml-network
 
 $(PREF_OBJ)%.o : $(PREF_SRC)%.cpp
 	$(CC) -c $< -o $@
--- SRC/main.cpp
+++ SRC/main.cpp
@@ -1,18 +1,34 @@
 #include <iostream>
+#include <string>
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
 
 using namespace std;
 
+const int width =  800;
+const int height = 800;
+
 int main(){
-    sf::RenderWindow window(sf::VideoMode({800, 800}), "Game");
+    sf::RenderWindow window(sf::VideoMode({width, height}), "Game");
 
     //Переменные
+    float radius = 100.f;
+
     sf::Texture texture;
-    
+    sf::Clock clock;
+    sf::Shader shader;
+    sf::CircleShape circle(radius);
+
 
     //Загрузка ресурсов
-    if (!texture.loadFromFile("STATIC/IMAGES/texture.jpg"))
+    if (!shader.loadFromFile("STATIC/SHADERS/fragment.frag", sf::Shader::Type::Fragment))
         return -1;
+
+    //Переменные для ресурсов
+
+
+    circle.setPosition({(int)width/2-radius, (int)height/2-radius});
 
     while (window.isOpen()){
         while (const std::optional event = window.pollEvent()){
@@ -20,7 +36,11 @@ int main(){
                 window.close();
         }
 
+        shader.setUniform("time", clock.getElapsedTime().asSeconds());
+
         window.clear();
+
+        window.draw(circle, &shader);
 
         window.display();
     }
